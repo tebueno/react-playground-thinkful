dfb8fce7c1e6a7e6439a2fbc15db5738f73c2f34
Tom Bueno <tombueno@toms-mbp.attlocal.net>
2020-02-20 07:11:29 -0800

    Adding a class component with default prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Split from './composition/Split';
+import Tooltip from './composition/TooltipClass';
 
-function App() {
-  return (
-    <main className="App">
-      <Split className="left">
-        This is my split component!
-      </Split>
-      <Split className="right">
-      This is my split component!
-      </Split>
-    </main>
-  );
-}
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
+  
+  function App() {
+    return (
+      <main className='App'>
+        <Split className='left' flexBasis='2'>
+          This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+          {/* make another tooltip directly inside the App */}
+          <Tooltip message='one more tooltip message'>
+            Necessitatibus?
+          </Tooltip>
+        </Split>
+        <Split className='right'>
+          This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+        </Split>
+      </main>
+    )
+  }
 
 export default App;
